86958f276e05b795b38d4ab3fd7f11002ebb2c2b
Anton Krug <anton.krug@microsemi.com>
2017-11-20 15:28:46 +0000

    Adding settings to wrong file

--- prepareDistribution.sh
+++ prepareDistribution.sh
@@ -27,7 +27,7 @@ chmod 0700 /home/vagrant/.ssh
 wget --no-check-certificate https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub -O /home/vagrant/.ssh/authorized_keys
 chmod 0600 /home/vagrant/.ssh/authorized_keys
 
-echo "AuthorizedKeysFile %h/.ssh/authorized_keys" | sudo tee -a /etc/apt/sources.list
+echo "AuthorizedKeysFile %h/.ssh/authorized_keys" | sudo tee -a /etc/ssh/sshd_config
 sudo /etc/init.d/ssh restart
 
 
