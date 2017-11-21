35ae1f246f945e7b095ce43253687e3125e15de5
Anton Krug <anton.krug@microsemi.com>
2017-11-21 10:42:21 +0000

    Using prefix variable

--- mingw-download-build-install.sh
+++ mingw-download-build-install.sh
@@ -2,12 +2,12 @@
 
 #anton krug 20.11.2017
 #inspired by:
+# https://sourceforge.net/p/mingw-w64/wiki2/Cross%20Win32%20and%20Win64%20compiler/
 # http://pete.akeo.ie/2010/07/compiling-mingw-w64-with-multilib-on.html
 # https://sourceforge.net/p/mingw-w64/mailman/message/35537983/
-# https://sourceforge.net/p/mingw-w64/wiki2/Cross%20Win32%20and%20Win64%20compiler/
 # http://redsymbol.net/articles/unofficial-bash-strict-mode/
  
-
+MGW_PREFIX=/opt/mingw
 VERSION=7.2.0
 THREADS=`grep -c ^processor /proc/cpuinfo`
 ARCH=`uname -m`
@@ -19,13 +19,12 @@ else
  ARCH=i686-linux-gnu
 fi
 
-echo GCC $VERSION building for $ARCH
+echo GCC $VERSION building for $ARCH into $MGW_PREFIX
 
 clean_install_dir() {
-    rm -rf /usr/local/x86_64-w64-mingw32 2> /dev/null
-    sudo mkdir /usr/local/x86_64-w64-mingw32 2> /dev/null
-    #sudo chown -R vagrant:vagrant /usr/local/x86_64-w64-mingw32 2> /dev/null
-    sudo chown -R vagrant:vagrant /usr/local
+    rm -rf $MGW_PREFIX 2> /dev/null
+    sudo mkdir $MGW_PREFIX 2> /dev/null
+    sudo chown -R vagrant:vagrant $MGW_PREFIX
 }
 
 clean_downloads() {
@@ -78,31 +77,35 @@ download() {
 build() {
     echo "------------------- build... ------------------"
 
+
     echo "------------------- binutils ------------------"
     mkdir ~/mingw-build/binutils-2.29/build
     cd ~/mingw-build/binutils-2.29/build
     set -euo pipefail 
     #../configure --prefix=/opt/mingw64 --target=x86_64-w64-mingw32 --enable-targets=x86_64-w64-mingw32,i686-w64-mingw32
     #../configure --target=x86_64-w64-mingw32 --enable-targets=x86_64-w64-mingw32,i686-w64-mingw32
-    ../configure --with-sysroot=/usr/local/ --prefix=/usr/local/ --target=x86_64-w64-mingw32 --enable-targets=x86_64-w64-mingw32,i686-w64-mingw32 --with-system-zlib
+    ../configure --with-sysroot=$MGW_PREFIX --prefix=$MGW_PREFIX --target=x86_64-w64-mingw32 --enable-targets=x86_64-w64-mingw32,i686-w64-mingw32 --with-system-zlib
     make -j$THREADS
     make install
     set +euo
-    
+
+
     echo "------------------- mingw headers ------------------"
     mkdir ~/mingw-build/mingw-w64-v5.0.3/build
     cd ~/mingw-build/mingw-w64-v5.0.3/build
     set -euo pipefail 
     #../mingw-w64-headers/configure --prefix=/opt/mingw64 --host=x86_64-w64-mingw32
     #../mingw-w64-headers/configure --host=x86_64-w64-mingw32
-    ../mingw-w64-headers/configure --build=x86_64-linux-gnu --prefix=/usr/local/x86_64-w64-mingw32 --host=x86_64-w64-mingw32
+    ../mingw-w64-headers/configure --prefix=$MGW_PREFIX/x86_64-w64-mingw32 --build=x86_64-linux-gnu --host=x86_64-w64-mingw32
     make install
     set +euo
 
+
     echo "------------------- symlinks ------------------"
-    mkdir -p /usr/local/x86_64-w64-mingw32/lib32
-    ln -s /usr/local/x86_64-w64-mingw32/lib /usr/local/x86_64-w64-mingw32/lib64
-    ln -s /usr/local/x86_64-w64-mingw32 /usr/local/mingw
+    mkdir -p $MGW_PREFIX/x86_64-w64-mingw32/lib32
+    ln -s $MGW_PREFIX/x86_64-w64-mingw32/lib $MGW_PREFIX/x86_64-w64-mingw32/lib64
+    ln -s $MGW_PREFIX/x86_64-w64-mingw32 $MGW_PREFIX/mingw
+
 
     echo "------------------- gcc 1st stage ------------------"
     mkdir ~/mingw-build/gcc-$VERSION/build
@@ -112,20 +115,22 @@ build() {
     #https://sourceforge.net/p/mingw-w64/mailman/mingw-w64-public/thread/4C884EE7.7060009@users.sourceforge.net/
     #../configure --with-sysroot=/usr/local/ --disable-nls --target=x86_64-w64-mingw32 --enable-languages=c,c++ --with-system-zlib --enable-multilib --enable-shared --enable-fully-dynamic-string
     #../configure --with-sysroot=/usr/local/ --prefix=/usr/local/ --disable-nls --target=x86_64-w64-mingw32 --enable-languages=c,c++ --with-system-zlib --enable-multilib --enable-shared --enable-fully-dynamic-string
-    ../configure --with-sysroot=/usr/local/ --prefix=/usr/local/ --disable-nls --target=x86_64-w64-mingw32 --enable-languages=c,c++ --with-system-zlib --enable-multilib  --enable-targets=all --enable-shared --enable-fully-dynamic-string
+    ../configure --with-sysroot=$MGW_PREFIX --prefix=$MGW_PREFIX --disable-nls --target=x86_64-w64-mingw32 --enable-languages=c,c++ --with-system-zlib --enable-multilib  --enable-targets=all --enable-shared --enable-fully-dynamic-string
     make all-gcc -j$THREADS
     #sudo make install-gcc
     make install-gcc
     set +euo
 
+
     echo "------------------- mingw crt ------------------"
     cd ~/mingw-build/mingw-w64-v5.0.3/build
     set -euo pipefail 
     #../configure --with-sysroot=/usr/local/ --prefix=/usr/local/x86_64-w64-mingw32 --disable-nls --host=x86_64-w64-mingw32 --enable-experimental --enable-lib32 
-    ../configure --with-sysroot=/usr/local/ --prefix=/usr/local/x86_64-w64-mingw32 --host=x86_64-w64-mingw32  
+    ../configure --prefix=$MGW_PREFIX/x86_64-w64-mingw32 --with-sysroot=/usr/local/ --host=x86_64-w64-mingw32  
     make -j$THREADS
     make install
     set +euo
+
 
     echo "------------------- gcc 2nd pass ------------------"
     cd ~/mingw-build/gcc-$VERSION/build
@@ -133,11 +138,10 @@ build() {
     make -j$THREADS
     make install
     set +euo
+}
 
-    #set -euo pipefail
-    #../gcc-$VERSION/configure --prefix=/opt/gcc-$VERSION -v --build=$ARCH --host=$ARCH --target=$ARCH --enable-checking=release --enable-languages=c,c++,fortran --disable-multilib --program-suffix=-$VERSION
-    #make -j $THREADS
-    #set +euo
+add_pernamently_to_bash_rc() {
+
 }
 
 #clean_downloads
@@ -147,3 +151,4 @@ clean_build
 
 download
 build
+add_pernamently_to_bash_rc
